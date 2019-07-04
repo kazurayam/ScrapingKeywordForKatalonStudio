989b8240fd56a8561caa2d7ad284434ad23048bf
kazurayam <>
2019-07-04 14:53:59 +0900

    refactored MailKeyword class: introduce Builder pattern

--- Include/scripts/groovy/com/kazurayam/katalon/keyword/MailKeywordTest.groovy
+++ Include/scripts/groovy/com/kazurayam/katalon/keyword/MailKeywordTest.groovy
@@ -1,4 +1,5 @@
 package com.kazurayam.katalon.keyword
+
 import static org.hamcrest.CoreMatchers.*
 import static org.hamcrest.MatcherAssert.*
 import static org.junit.Assert.*
@@ -9,8 +10,12 @@ import org.junit.runner.RunWith
 
 import com.kazurayam.junit4ks.IgnoreRestSupportRunner
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 /**
+ * TODO
  * ユニットテスト用のメールサーバsubethasmtpがあることを知りました
  * https://qiita.com/tchofu/items/79097b68cc9e22a1f401
  * 
@@ -21,16 +26,39 @@ import com.kazurayam.junit4ks.IgnoreRestSupportRunner
 @RunWith(IgnoreRestSupportRunner.class)
 public class MailKeywordTest {
 
+    static Path katalonPropsPath
+    static Properties katalonProps
+    
     @BeforeClass public static void onlyOnce() {
+        katalonPropsPath = Paths.get('./katalon.properties')
+        if (!Files.exists(katalonPropsPath)) {
+            throw new IOException("${katalonPropsPath} is not found")
+        }
+        katalonProps = new Properties()
+        katalonProps.load(new FileInputStream(katalonPropsPath.toFile()))
     }
 
     @Test
     void test_sendMail() {
         // setup:
-        MailKeyword keyword = new MailKeyword()
+        String host = katalonProps.getProperty('MailKeyword.host',
+                                    "host is not defined in ${katalonPropsPath}")
+        String port = katalonProps.getProperty('MailKeyword.port',
+                                    "port is not defined in ${katalonPropsPath}")
+        String senderAddress = katalonProps.getProperty('MailKeyword.senderAddress',
+                                    "MailKeyword.senderAddress is not defined in ${katalonPropsPath}")
+        String senderPassword = katalonProps.getProperty('MailKeyword.senderPassword',
+                                    "MailKeyword.senderPassword is not defined in ${katalonPropsPath}")
+        MailKeyword keyword = new MailKeyword.Builder(host, port)
+                                    .senderAddress('kazuaki.matsuhashi@quick.jp')
+                                    .senderPassword('Wakako1Akihioro2Hanae4')
+                                    .build()
         // when:
-        boolean success = keyword.sendMail("Hello, world")
+        String receiverAddress = "kazuaki.matsuhashi@quick.jp"
+        String subject = "Test test"
+        String message = """<h1>Hello, world</h1>"""
+        boolean success = keyword.sendMail(receiverAddress, subject, message);
         // then:
-        assertThat(success, is(true))
+        assertThat(success, is(true));
     }
 }
--- Keywords/com/kazurayam/katalon/keyword/MailKeyword.groovy
+++ Keywords/com/kazurayam/katalon/keyword/MailKeyword.groovy
@@ -14,42 +14,108 @@ import com.kms.katalon.core.annotation.Keyword
 
 public class MailKeyword {
 
+    private String senderAddress;
+    private String senderPassword;
+    private String host;
+    private String port;
+
+    public static class Builder {
+        private String senderAddress;
+        private String senderPassword;
+        private String host;
+        private String port;
+        public Builder(String host, String port) {
+            Objects.requireNonNull(host, "host must not be null");
+            Objects.requireNonNull(port, "port must not be null")
+            if (host.length() == 0) {
+                throw new IllegalArgumentException("host must not be an empty string");
+            }
+            
+            if (port.length() == 0) {
+                throw new IllegalArgumentException("port must not be an empty string")
+            }
+            this.host = host
+            this.port = port
+        }
+        public Builder senderAddress(String senderAddress) {
+            Objects.requireNonNull(senderAddress, "senderAddress must not be null");
+            if (senderAddress.length() == 0) {
+                throw new IllegalArgumentException("senderAddress must not be an empty string");
+            }
+            this.senderAddress = senderAddress;
+            return this;
+        }
+        public Builder senderPassword(String senderPassword) {
+            Objects.requireNonNull(senderPassword, "senderPassword must not be null");
+            if (senderPassword.length() == 0) {
+                throw new IllegalArgumentException("senderPassword must not be an empty string");
+            }
+            this.senderPassword = senderPassword;
+            return this;
+        }
+        public MailKeyword build() {
+            return new MailKeyword(this);
+        }
+    }
+
+
+    /**
+     * 
+     * @param builder
+     */
+    private MailKeyword(Builder builder) {
+        this.senderAddress = builder.senderAddress;
+        this.senderPassword = builder.senderPassword;
+        this.host = builder.host;
+        this.port = builder.port;
+    }
+
     /**
      * 
      * @param htmlMessage
      * @return true if sent message successfully.
      */
     @Keyword
-    public static boolean sendMail(String htmlMessage) {
-        final String username = "kazuaki.matsuhashi@quick.jp";
-        final String password = "Wakako1Akihiro2Hanae4";
-
+    public boolean sendMail(
+            String receiverAddress,
+            String subject,
+            String htmlMessage,
+            String mimeType = 'text/html; charset=iso-2022-jp') {
+        Objects.requireNonNull(receiverAddress, "receiverAddress must not be null")
+        Objects.requireNonNull(subject, "subject must not be null")
+        Objects.requireNonNull(htmlMessage, "htmlMessage must not be null")
+        Objects.requireNonNull(mimeType, "mimeType must not be null")
+        if (this.senderAddress == null) {
+            throw new IllegalStateException("senderAddress is null. Please set it by calling MailKeyword.Builder.senderAddress(String)")
+        }
+        if (this.senderPassword == null) {
+            throw new IllegalStateException("senderPassword is null. Please set it by calling MailKeyword.Builder.senderPassword(String)")
+        }
+        if (this.host == null) {
+            throw new IllegalStateException("host is null. Please set it by calling MailKeyword.Builder.host(String)")
+        }
+        if (this.port == null) {
+            throw new IllegalStateException("port is null. Please set it by calling MailKeyword.Builder.port(String)")
+        }
         Properties props = new Properties();
         props.put("mail.smtp.auth", true);
         props.put("mail.smtp.starttls.enable", true);
-        props.put("mail.smtp.host", "172.24.172.4");
-        props.put("mail.smtp.port", "25");
-
+        props.put("mail.smtp.host", this.host);
+        props.put("mail.smtp.port", this.port);
         Session session = Session.getInstance(props,
                 new Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
+                        return new PasswordAuthentication(this.senderAddress, this.senderPassword);
                     }
                 });
-
         try {
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
+            message.setFrom(new InternetAddress(this.senderAddress));
             message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("kazuaki.matsuhashi@quick.jp"));
-            message.setSubject("Test warning");
-            message.setContent(htmlMessage, "text/html; charset=iso-2022-jp");
-
-            System.out.println("Sending...");
-
+                    InternetAddress.parse(receiverAddress));
+            message.setSubject(subject);
+            message.setContent(htmlMessage, mimeType);
             Transport.send(message);
-
-            System.out.println("Sent.");
         } catch (SendFailedException e) {
             e.printStackTrace();
             return false;
