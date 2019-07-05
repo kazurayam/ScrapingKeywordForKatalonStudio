dff0330af7815ba118e098849101363da8de2ab1
kazurayam <>
2019-07-05 16:03:56 +0900

    introduced Sub Etha SMTP to mock STMP Server

--- Include/scripts/groovy/com/kazurayam/katalon/keyword/MailSenderTest.groovy
+++ Include/scripts/groovy/com/kazurayam/katalon/keyword/MailSenderTest.groovy
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.*
 import static org.hamcrest.MatcherAssert.*
 import static org.junit.Assert.*
 
+import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,51 +15,79 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
+import javax.mail.MessagingException
+import org.subethamail.wiser.Wiser
+import org.subethamail.wiser.WiserMessage
+import javax.mail.internet.MimeMessage
+
 /**
  * TODO
  * ユニットテスト用のメールサーバsubethasmtpがあることを知りました
  * https://qiita.com/tchofu/items/79097b68cc9e22a1f401
  * 
+ * Here we use Subethamail at https://github.com/voodoodyne/subethasmtp to mock a STMP server
  * 
- * @author qcq0264
+ * @author kazuarayam
  *
  */
 @RunWith(IgnoreRestSupportRunner.class)
 public class MailSenderTest {
 
-    static Path katalonPropsPath
-    static Properties katalonProps
+    private static Wiser wiser
+    private static String smtpHost
+    private static String smtpPort
+    private static Path katalonPropsPath
+    private static Properties katalonProps
 
-    @BeforeClass public static void onlyOnce() {
-        katalonPropsPath = Paths.get('./katalon.properties')
-        if (!Files.exists(katalonPropsPath)) {
-            throw new IOException("${katalonPropsPath} is not found")
-        }
-        katalonProps = new Properties()
-        katalonProps.load(new FileInputStream(katalonPropsPath.toFile()))
+    @BeforeClass public static void onlyOnce() throws Exception {
+        // start up a Mail server
+        smtpHost = 'localhost'
+        smtpPort = '2500'
+        wiser = new Wiser()
+        wiser.setHostname(smtpHost)
+        wiser.setPort(Integer.parseInt(smtpPort))
+        wiser.start()
+    }
+    
+    @AfterClass public static void tearDownAfterClass() throws Exception {
+        // stop the mail server
+        wiser.stop()
     }
 
     @Test
     void test_sendMail() {
         // setup:
-        String host = katalonProps.getProperty('MailSender.host',
-                "host is not defined in ${katalonPropsPath}")
-        String port = katalonProps.getProperty('MailSender.port',
-                "port is not defined in ${katalonPropsPath}")
-        String senderAddress = katalonProps.getProperty('MailSender.senderAddress',
-                "MailKeyword.senderAddress is not defined in ${katalonPropsPath}")
-        String senderPassword = katalonProps.getProperty('MailSender.senderPassword',
-                "MailKeyword.senderPassword is not defined in ${katalonPropsPath}")
-        MailSender sender = new MailSender.Builder(host, port)
-                .senderAddress('kazuaki.matsuhashi@quick.jp')
-                .senderPassword('Wakako1Akihioro2Hanae4')
+        String senderAddress  = 'mick.jagger@rollingstones.com'
+        String senderPassword = 'ThisIsAFakePassword'
+        MailSender sender = new MailSender.Builder(smtpHost, smtpPort)
+                .senderAddress(senderAddress)
+                .senderPassword(senderPassword)
                 .build()
         // when:
-        String receiverAddress = "kazuaki.matsuhashi@quick.jp"
+        String receiverAddress = "keith.richards@rollingstones.com"
         String subject = "Test test"
         String message = """<h1>Hello, world</h1>"""
         boolean success = sender.sendMail(receiverAddress, subject, message);
         // then:
         assertThat(success, is(true));
+        
+        // verify the content of mail sent
+        List<WiserMessage> messages = wiser.getMessages()
+        for (WiserMessage msg: messages) {
+            assertThat(msg.getEnvelopeSender(), is(senderAddress))
+            assertThat(msg.getEnvelopeReceiver(), is(receiverAddress))
+            try {
+                assertThat(msg.getMimeMessage().getSubject(), is(subject))
+                MimeMessage mimeMsg = msg.getMimeMessage()
+                assertThat(mimeMsg.getContentType(), is('text/html; charset=iso-2022-jp'))
+                if (mimeMsg.getContent() instanceof String) {
+                    String content = (String)mimeMsg.getContent()
+                    assertThat(content.trim(), is(message.trim()))
+                }
+            } catch(MessagingException e) {
+                fail('Couldnt fine a Subject')
+            }
+        }
+        
     }
 }
